c92373e80c4c60c760d1d19f7b63ac1e5091452e
prajakta-shiradkar <71390191+prajakta-shiradkar@users.noreply.github.com>
2020-09-17 21:54:49 +0530

    Update A.java

--- GitDemoProject/src/A.java
+++ GitDemoProject/src/A.java
@@ -5,6 +5,10 @@ public class A
 	{
 		System.out.println("sum--A");
 	}
+	public void sub()
+	{
+		System.out.println("sub--A");
+	}
 	public static void main(String[] args) 
 	{
 		System.out.println("praju");
